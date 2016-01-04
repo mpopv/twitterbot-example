8d9142161c69ee6b8aee635c17f3b88fd51135c2
Matt Popovich <mattpopovich@outlook.com>
2016-01-04 14:19:21 -0500

    Making numbers into phrases

--- bot.js
+++ bot.js
@@ -8,11 +8,18 @@ var Bot = new TwitterBot({
  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
-var numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20];
-var number = chooseRandom(numberArray);
-
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-Bot.tweet(number);
+var animalArray = ["i like cat",
+                   "dog is good pet",
+                   "sushi fish yum",
+                   "banana",
+                   "horse dot com slash horsesarecool",
+                   "starfish"
+                 ];
+                 
+var animal = chooseRandom(animalArray);
+
+Bot.tweet(animal);
