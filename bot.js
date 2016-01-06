142df5899cb9a844b3bb2ef5cd137aaceb3f4d6e
Matt Popovich <mattpopovich@outlook.com>
2016-01-06 15:12:41 -0500

    Testing number array

--- bot.js
+++ bot.js
@@ -12,31 +12,33 @@ function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-var animalArray = ["i like cat",
-                   "dog is good pet",
-                   "sushi fish yum",
-                   "banana",
-                   "horse dot com slash horsesarecool",
-                   "starfish",
-                   "rsjn oin oiq nroiuqg iu",
-                   "DRINK MORE OVALTINE",
-                   "frog blast the vent core",
-                   "somebody set up us the bomb",
-                   "http://bomb.com",
-                   "use the force luke",
-                   "sentence",
-                   "s",
-                   "e",
-                   "n",
-                   "t",
-                   "koala bear",
-                   "orca",
-                   "dishwasher",
-                   "placeholder",
-                   "hi",
-                   "lol",
-                   "lmao"
-                  ];
+// var animalArray = ["i like cat",
+//                    "dog is good pet",
+//                    "sushi fish yum",
+//                    "banana",
+//                    "horse dot com slash horsesarecool",
+//                    "starfish",
+//                    "rsjn oin oiq nroiuqg iu",
+//                    "DRINK MORE OVALTINE",
+//                    "frog blast the vent core",
+//                    "somebody set up us the bomb",
+//                    "http://bomb.com",
+//                    "use the force luke",
+//                    "sentence",
+//                    "s",
+//                    "e",
+//                    "n",
+//                    "t",
+//                    "koala bear",
+//                    "orca",
+//                    "dishwasher",
+//                    "placeholder",
+//                    "hi",
+//                    "lol",
+//                    "lmao"
+//                   ];
+
+var animalArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20];
 
 var animal = chooseRandom(animalArray);
 
